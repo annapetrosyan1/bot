2d57f80838cec7e82e798e8b19ac3cbab25646e4
17N71 <narek07012020@gmail.com>
2023-03-31 02:50:12 +0400

    locale db and uje chem hishum

--- db/knex.js
+++ db/knex.js
@@ -1,31 +1,34 @@
-require('dotenv').config();
-
+require('dotenv').config()
+const axios = require('axios')
+const { list } = require('../source/bot')
 const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    },
-    pool: {min: 2, max: 10}
-});
+	client: 'pg',
+	connection: {
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+	},
+	pool: { min: 2, max: 10 },
+})
 
-let usersList = new Object();
+const usersList = {}
+for (row of list) {
+	axios
+		.post('http://localhost:3000/emails', {
+			email: row,
+		})
+		.then(resp => {
+			console.log(resp.data)
+		})
+		.catch(error => {
+			console.log(error)
+		})
+}
 
-knex.from('users').select("id", "email")
-    .then((rows) => {
-        for (row of rows) {
-            usersList.id = row['id'];
-            usersList.email = row['email']
-            console.log(usersList)
-        }
-    })
-
-// //проверка подключения
+// проверка подключения
 // knex.raw('SELECT VERSION()').then(() => {
 //     console.log('connection to db successfull');
 // });
-
-module.exports = knex;
+module.exports = knex
--- source/bot.js
+++ source/bot.js
@@ -1,36 +1,37 @@
-require('dotenv').config();
+require('dotenv').config()
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+const axios = require('axios')
+const { v4: uuidv4 } = require('uuid')
+const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.[a-zA-Z]{2,10})+$/gi
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start(ctx => ctx.reply('Для авторизации введите свой email: '))
 
-const { Telegraf } = require('telegraf');
-const axios = require('axios');
-const knex = require('../db/knex');
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-bot.start((ctx) => ctx.reply('Для авторизации введите свой email: '));
-
-// bot.on('message', async (msg) => {
-    
-//   const text = msg.text;
-//   const chatId = msg.chat_id;
-
-//   try {
-//     if (text === 'мой_айди') { return bot.sendMessage(chatId, `Твой айди: ${chatId}`) }
-//   } catch (e) {
-//     console.log('Подключение к БД сломалось')
-//   }
-
-// })
-
-bot.on('edited_message', ctx => {
-  ctx.reply('Вы успешно изменили сообщение')
+bot.on(message('start'), async ctx => {
+	const email = await ctx.message.text
+	if (emailRegExp.test(email)) {
+		axios
+			.post('http://localhost:3000/emails', {
+				id: uuidv4,
+				email,
+			})
+			.then(resp => {
+				console.log(resp.data)
+			})
+			.catch(error => {
+				console.log(error)
+			})
+		ctx.reply(`Вы успешно емайл ${email}`)
+		return ''
+	}
+	ctx.reply(`Вы писали неверный емайл`)
 })
 
 // bot.on('мой_id', ctx => {
 //   let user__id = ctx.from_user.id;
 //   ctx.reply('Твой ID: ' + user__id)
 // })
-  
-bot.launch();
+bot.launch()
 
 // a)	Создать бота со следующим функционалом
 // i)	При стартер бот запрашивает email
